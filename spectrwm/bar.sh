a1a6e27bbd4b47cc25df07850e5aafbcd4a1fb17
Anthony Delannoy <anthony@spnngl.com>
2016-03-20 21:06:32 +0100

    few changes bar.sh

--- spectrwm/bar.sh
+++ spectrwm/bar.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/zsh
 
 # my simple bar
+CONNECTION="wlp3s0"
+#CONNECTION=$(iwgetid | awk '{print $1}')
+DATEFORMAT='+ %a %d %b %R'
+DODO=5
 
 function battery() {
     LVL=$(acpi | awk '{print $4}')
@@ -9,18 +13,23 @@ function battery() {
 }
 
 function curtime() {
-    TIME=$(date '+ %a %d %b %R')
+    TIME=$(date $DATEFORMAT)
     echo -n "$TIME |"
 }
 
+function freeram() {
+    AVRAM=$(free -kh | grep Mem: | awk '{print $7}')
+    echo -n "$AVRAM |"
+}
+
 function ipv4() {
-    CONNECTION="wlp3s0"
-    ESSID=$(iwconfig $CONNECTION | grep ESSID: | awk '{print $4}')
-    IP=$(ip addr show $CONNECTION | grep inet | awk '{print $2}' | head -n1 | sed 's|/24||')
+    #ESSID=$(iwconfig $CONNECTION | grep ESSID: | awk '{print $4}')
+    ESSID=$(iwgetid -r)
+    IP=$(ip addr show $CONNECTION | grep inet | awk '{print $2}' | head -n1)
     echo -n "$ESSID | $IP |"
 }
 
 while :; do
-    echo "$(curtime) $(ipv4) $(battery)"
-    sleep 2
+    echo "$(curtime) $(ipv4) $(freeram) $(battery)"
+    sleep $DODO
 done
